836c9168b4521effc98c2af730e920b48fdc3865
mhdRaihan321 <mhdraihan383@gmail.com>
2024-12-26 12:25:05 +0530

    Case Changed

--- routes/User_Route.js
+++ routes/User_Route.js
@@ -8,8 +8,8 @@ router.post('/register-user',Register)
 router.post('/login-user',Login)
 router.post('/fetch-sms-bank', Fetch_SMS_FROM_PHONE)
 router.get('/get-all-info-fetched-sms-bank', getAllMoneyTrackerData);
-router.get('/get-all-Debited', allDebited)
-router.get('/get-all-Credited', allCredited)
+router.get('/get-all-debited', allDebited)
+router.get('/get-all-credited', allCredited)
 router.get('/get-user',authenticate, (req,res)=>{
     res.status(200).json({
         status:true,
